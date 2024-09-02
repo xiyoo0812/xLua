bdba5c6a812a9bf35889dad9b62dae528f095681
xiyoo0812 <xiyoo0812@163.com>
2024-09-02 17:36:50 +0800

    整理xlua

--- build/make_win_lua54.bat
+++ build/make_win_lua54.bat
@@ -26,11 +26,4 @@ cmake --build build64_54 --config Release
 md plugin_lua54\Plugins\x86_64
 copy /Y build64_54\Release\xlua.dll plugin_lua54\Plugins\x86_64\xlua.dll
 
-mkdir build32_54 & pushd build32_54
-cmake -DLUA_VERSION=5.4.1 -G "%__VS%" -A Win32 ..
-popd
-cmake --build build32_54 --config Release
-md plugin_lua54\Plugins\x86
-copy /Y build32_54\Release\xlua.dll plugin_lua54\Plugins\x86\xlua.dll
-
 pause
