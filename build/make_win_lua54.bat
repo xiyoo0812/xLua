1cccdac5dd332d0ec4a85c8b5ec5b71cc851f769
xiyoo0812 <xiyoo0812@163.com>
2025-06-25 23:06:55 +0800

    升级5.4.6

--- build/make_win_lua54.bat
+++ build/make_win_lua54.bat
@@ -20,7 +20,7 @@ if "%__VSVER%" neq "" (
 
 
 mkdir build64_54 & pushd build64_54
-cmake -DLUA_VERSION=5.4.1 -G "%__VS%" -A x64  ..
+cmake -DLUA_VERSION=5.4.6 -G "%__VS%" -A x64  ..
 popd
 cmake --build build64_54 --config Release
 md plugin_lua54\Plugins\x86_64
